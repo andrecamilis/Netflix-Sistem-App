98026cba7bfda0dc509a18f13c22b4be881752c8
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-02-26 09:35:15 -0300

    classes created
    Serie, Categoria and Episodio class created

---
+++ app/src/main/java/com/example/testenetflix/model/Categoria.java
@@ -0,0 +1,8 @@
+package com.example.testenetflix.model;
+
+public class Categoria {
+
+    private int id_categoria;
+    private String nome_categoria;
+
+}
---
+++ app/src/main/java/com/example/testenetflix/model/Episodio.java
@@ -0,0 +1,12 @@
+package com.example.testenetflix.model;
+
+public class Episodio {
+
+    private int id_episodio;
+    private int titulo;
+    private int numero_episodio;
+    private int numero_temporada;
+    private String uri_imagem;
+    private int duracao;
+
+}
